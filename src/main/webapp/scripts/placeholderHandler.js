0a477da7a24a911e045145dd411211fb251231f5
vladislavmedvedev <EnoT3581@mail.ru>
2024-04-06 22:05:31 +0300

    made some fixes

--- src/main/webapp/scripts/placeholderHandler.js
+++ src/main/webapp/scripts/placeholderHandler.js
@@ -7,6 +7,9 @@ for (var i = 0; i < playerInputs.length; i++) {
 
     playerInputs[i].addEventListener("blur", function () {
         if (this.value === "") {
+            if(i == 2){
+                i = 0;
+            }
             this.setAttribute("placeholder", "Enter the name of player " + (i + 1));
         }
     });
